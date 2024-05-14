228f45f216581e1543a18fd9af0487a99d347e68
Sam Jones <sam@refractar.com>
2024-05-14 13:49:13 -0600

    new loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -12,21 +12,31 @@ export const Loading = ({ onStartButtonClick, camKitLoaded }: Props) => {
           <li>
             <span>1</span>
             <img src="magnifying-glass.png" />
-            <p>enable motion permission</p>
+            <p>Find a flat surface & enable motion permissions</p>
           </li>
 
           <li>
             <span>2</span>
             <img src="camera.png" />
-            <p>Aim your phone camera at the correct images</p>
+            <p>Aim your phone camera at the hoop</p>
           </li>
           <li>
             <span>3</span>
-            <img src="tap.png" />
-            <p>Solve the puzzel!</p>
+            <img src="power.png" />
+            <p>Use the power bar to shoot</p>
+          </li>
+          <li>
+            <span>4</span>
+            <img src="repeat.png" />
+            <p>Score and repeat</p>
           </li>
         </ol>
         <br />
+        <p>If you win the AR game, you'll win:</p>
+        <article>
+          <img src="ar.png" />
+          FREE Game-Day Ticket
+        </article>
 
         <button
           disabled={!camKitLoaded}
